46f72c2e5f81c4ea9f6fa22002f029812ed18004
Acalinei Beniamin <beniacalinei1@yahoo.com>
2020-11-21 17:10:51 +0200

    About Us Page Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutUsPageComponent } from './components/about-us-page/about-us-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { MatCardTitle} from "@angular/material/card";
+import { MatCardHeader} from "@angular/material/card";
+import { MatCard} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
